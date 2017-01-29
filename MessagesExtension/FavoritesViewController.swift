844928bec980b55b8c86d7732e561041a902d85a
MC_Doshi <doshi.michael@gmail.com>
2017-01-28 16:39:38 -0800

    Added FavoritesViewController file

---
+++ MessagesExtension/FavoritesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoritesViewController.swift
+//  meetchewthere
+//
+//  Created by Michael-Anthony Doshi on 1/28/17.
+//  Copyright © 2017 Alejandrina Gonzalez Reyes. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
