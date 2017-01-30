0e3ec2ea2995f001bcbed9dde1041fd305504c10
MC_Doshi <doshi.michael@gmail.com>
2017-01-30 15:06:36 -0800

    Added basic swipe functionality files Included restaurant image in messages

--- MessagesExtension/VotingViewController.swift
+++ MessagesExtension/VotingViewController.swift
@@ -12,24 +12,30 @@ class VotingViewController: UIViewController {
     
     // MARK: - Properties
     
+    private lazy var swipeView: ZLSwipeableView = {
+        let swipeView = ZLSwipeableView()
+        swipeView.numberOfActiveView = 1
+        return swipeView
+    }()
+    
     private var sharedRestaurants: [Int] = []
+    private var restIndex = 0
+    private let data = Data()
     
     // MARK: - VotingViewController
     
     init(_ url: URL?) {
         if let url = url, let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false), let queryItems = urlComponents.queryItems {
             
-            var index = 0
             for queryItem in queryItems {
+                //guard let value = queryItem.value else { continue }
+    
                 guard let stringValue = queryItem.value,
                     let intValue = Int(stringValue) else {
                         continue
                 }
-                
-                print("Restaurant number \(intValue)")
-
+                print("Restaurant value: \(intValue)")
                 sharedRestaurants.append(intValue)
-                index += 1
             }
         }
         
@@ -39,10 +45,52 @@ class VotingViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func viewDidLoad() {
+    
+    /*override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(swipeView.usingAutolayout())
+        setUpConstraints()
+        
+    }*/
+    
+    /*override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        swipeView.nextView = {
+            return self.nextCardView()
+        }
+        swipeView.setNeedsLayout()
+    }*/
+    
+    // MARK: - Helper Methods
+    
+    private func setUpConstraints() {
+        
+        // swipeView constraints
+        NSLayoutConstraint.activate([
+            swipeView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 20.0),
+            swipeView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20.0),
+            swipeView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20.0),
+            swipeView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -20.0)
+            ])
     }
+    
+    /*func nextCardView() -> UIView? {
+        if restIndex < sharedRestaurants.count {
+            let cardView = CardView(frame: swipeView.bounds)
+            cardView.backgroundColor = .black
+            let restImageView = UIImageView(image: UIImage(named: sharedRestaurants[restIndex]))
+            restIndex += 1
+            cardView.addSubview(restImageView.usingAutolayout())
+            restImageView.clipsToBounds = true
+            NSLayoutConstraint.activate([
+                restImageView.leadingAnchor.constraint(equalTo: cardView.leadingAnchor),
+                restImageView.trailingAnchor.constraint(equalTo: cardView.trailingAnchor),
+                restImageView.centerXAnchor.constraint(equalTo: cardView.centerXAnchor),
+                restImageView.centerYAnchor.constraint(equalTo: cardView.centerYAnchor)
+                ])
+            return cardView
+        } else {
+            return nil
+        }
+    }*/
 }
